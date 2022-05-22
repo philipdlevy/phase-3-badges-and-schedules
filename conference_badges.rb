a788235a1969f98111d53e2dac08ec0e0383c94b
Philip Levy <philipdlevy@gmail.com>
2022-05-22 13:47:18 -0600

    commit tree 39845f61054e43c55ca99834df4a57d58ae4fdfc

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+badge_maker("John")
+
+
+def batch_badge_creator(array)
+    new_array = array.map do |name|
+        "Hello, my name is #{name}."
+    end
+    new_array
+end
+
+
+def assign_rooms(array)
+    new_array = array.map.with_index(1) do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+    new_array
+end
+
+
+def printer(array)
+    new_array = array.map do |name|
+        puts "Hello, my name is #{name}."
+    end
+    new_array2 = array.map.with_index(1) do |name, index|
+        puts "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+    new_array
+    new_array2
+end
